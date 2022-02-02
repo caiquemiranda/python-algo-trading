702f9d1387d658c515a6e5746511eed73f4ba177
caiquemiranda <caiquemirandas@gmail.com>
2022-02-02 09:13:32 -0300

    criação pasta analises

---
+++ carteira-ativos/carteira-ativos.py
@@ -0,0 +1,11 @@
+# import libs
+from datetime import datetimete
+import pandas as pd
+import numpy as np
+import requests
+import matplotlib.pyplot as plt
+import yfinance as yf
+from bs4 import BeautifulSoup
+import seaborn as sns
+
+pass
